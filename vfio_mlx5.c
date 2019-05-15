1792e7cdaef61bc6822bb27f40b0b90fb732ef39
Ido Shamay <idoshamay@gmail.com>
2019-05-15 18:54:44 +0300

    vfio_mlx5: Dedicated function for mlx5 device open
    Also created the initial struct that will store all the
    relevant device context once we will start to work with
    the device.
    
    Signed-off-by: Ido Shamay <idoshamay@gmail.com>

--- vfio_mlx5.c
+++ vfio_mlx5.c
@@ -1,10 +1,11 @@
 /*
- * test.c
+ * vfio_mlx5.c
  *
  *  Created on: May 15, 2019
  *      Author: idos
  */
 #include <stdio.h>
+#include <stdint.h>
 #include <string.h>
 #include <errno.h>
 #include <fcntl.h>
@@ -13,22 +14,30 @@
 #include <linux/vfio.h>
 #include <linux/limits.h>
 
+
+struct vfio_mlx5_device {
+    int group_id;
+    const char *bdf;
+    void *iseg;
+    uint64_t bar_size;
+};
+
+
 void usage(char *name)
 {
     printf("usage: %s <iommu group id> <ssss:bb:dd.f>\n", name);
 }
 
-int main (int argc, char **argv)
+
+int vfio_mlx5_device_open(struct vfio_mlx5_device *dev)
 {
-    int i;
     int ret;
     int container;
     int group;
     int device;
-    int groupid;
     int seg;
     int bus;
-    int dev;
+    int devid;
     int func;
     char path[PATH_MAX];
 
@@ -44,31 +53,20 @@ int main (int argc, char **argv)
         .argsz = sizeof(region_info)
     };
 
-    if (argc < 3) {
-        usage(argv[0]);
-        return -1;
-    }
-
-    ret = sscanf(argv[1], "%d", &groupid);
-    if (ret != 1) {
-        usage(argv[0]);
-        return -1;
-    }
-
-    ret = sscanf(argv[2], "%04x:%02x:%02x.%d", &seg, &bus, &dev, &func);
+    ret = sscanf(dev->bdf, "%04x:%02x:%02x.%d", &seg, &bus, &devid, &func);
     if (ret != 4) {
-        usage(argv[0]);
+        printf("Failed to parse BDF\n");
         return -1;
     }
 
     printf("Using PCI device %04x:%02x:%02x.%d in group %d\n",
-           seg, bus, dev, func, groupid);
+           seg, bus, devid, func, dev->group_id);
 
     container = open("/dev/vfio/vfio", O_RDWR);
     if (container < 0) {
         printf("Failed to open /dev/vfio/vfio, %d (%s)\n",
                 container, strerror(errno));
-        return container;
+        return -1;
     }
 
     ret = ioctl(container, VFIO_GET_API_VERSION);
@@ -77,18 +75,18 @@ int main (int argc, char **argv)
         return -1;
     }
 
-    snprintf(path, sizeof(path), "/dev/vfio/%d", groupid);
+    snprintf(path, sizeof(path), "/dev/vfio/%d", dev->group_id);
     group = open(path, O_RDWR);
     if (group < 0) {
         printf("Failed to open %s, %d (%s)\n",
                path, group, strerror(errno));
-        return group;
+        return -1;
     }
 
     ret = ioctl(group, VFIO_GROUP_GET_STATUS, &group_status);
     if (ret) {
         printf("ioctl(VFIO_GROUP_GET_STATUS) failed\n");
-        return ret;
+        return -1;
     }
 
     if (!(group_status.flags & VFIO_GROUP_FLAGS_VIABLE)) {
@@ -122,10 +120,10 @@ int main (int argc, char **argv)
     ret = ioctl(container, VFIO_SET_IOMMU, VFIO_TYPE1_IOMMU);
     if (ret) {
         printf("Failed to set IOMMU\n");
-        return ret;
+        return -1;
     }
 
-    snprintf(path, sizeof(path), "%04x:%02x:%02x.%d", seg, bus, dev, func);
+    snprintf(path, sizeof(path), "%04x:%02x:%02x.%d", seg, bus, devid, func);
     device = ioctl(group, VFIO_GROUP_GET_DEVICE_FD, path);
     if (device < 0) {
         printf("Failed to get device %s\n", path);
@@ -140,34 +138,60 @@ int main (int argc, char **argv)
     printf("Device supports %d regions, %d irqs 0x%x flags\n",
            device_info.num_regions, device_info.num_irqs, device_info.flags);
 
-    for (i = 0; i < device_info.num_regions; i++) {
-        printf("Region %d: ", i);
-        region_info.index = i;
-        if (ioctl(device, VFIO_DEVICE_GET_REGION_INFO, &region_info)) {
-            printf("Failed to get info\n");
-            continue;
-        }
+    region_info.index = 0;
+    if (ioctl(device, VFIO_DEVICE_GET_REGION_INFO, &region_info)) {
+        printf("Failed to get info for Region 0\n");
+        return -1;
 
-        printf("size 0x%lx, offset 0x%lx, flags 0x%x\n",
-               (unsigned long)region_info.size,
-               (unsigned long)region_info.offset, region_info.flags);
-        if (region_info.flags & VFIO_REGION_INFO_FLAG_MMAP) {
-            void *map = mmap(NULL, (size_t)region_info.size,
-                     PROT_READ, MAP_SHARED, device,
-                     (off_t)region_info.offset);
-            if (map == MAP_FAILED) {
-                printf("mmap failed\n");
-                continue;
-            }
-
-            printf("[");
-            fwrite(map, 1, region_info.size > 16 ? 16 :
-                        region_info.size, stdout);
-            printf("]\n");
-            munmap(map, (size_t)region_info.size);
-        }
     }
 
+    printf("size 0x%lx, offset 0x%lx, flags 0x%x\n",
+           (unsigned long)region_info.size,
+           (unsigned long)region_info.offset, region_info.flags);
+
+    if (!(region_info.flags & VFIO_REGION_INFO_FLAG_MMAP)) {
+        printf("Unexpected no mmap support for Region 0 in device\n");
+        return -1;
+    }
+
+    void *map = mmap(NULL, (size_t)region_info.size, PROT_READ, MAP_SHARED,
+                     device, (off_t)region_info.offset);
+
+    if (map == MAP_FAILED) {
+        printf("mmap failed for region 0\n");
+        return -1;
+    }
+
+    dev->bar_size = (unsigned long)region_info.size;
+    dev->iseg = map;
+    return 0;
+}
+
+
+int main (int argc, char **argv)
+{
+    int groupid;
+    struct vfio_mlx5_device dev;
+
+    if (argc < 3) {
+        usage(argv[0]);
+        return -1;
+    }
+
+    if (sscanf(argv[1], "%d", &groupid) != 1) {
+        usage(argv[0]);
+        return -1;
+    }
+
+    memset(&dev, 0, sizeof(struct vfio_mlx5_device));
+    dev.group_id = groupid;
+    dev.bdf = argv[2];
+    if (vfio_mlx5_device_open(&dev)) {
+        printf("Error opening mlx5 device using vfio-pci\n");
+        return -1;
+    }
+
+    munmap(dev.iseg, (size_t)dev.bar_size);
     return 0;
 }
 
